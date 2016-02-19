771aa4d39e6c9e449282513dd28b5946912c9fe2
Conan Yuan <conanyuan+cloud9@gmail.com>
2016-02-19 04:01:09 +0000

    read url params

--- main.js
+++ main.js
@@ -12,16 +12,44 @@ if (projects.length == 0) {
   console.log("Urls: "+projects);
 }
 
+function getJsonUrlParams(url, hashBased) {
+  var query;
+  var pos = url.indexOf(":", 5);
+  if(pos==-1) return [];
+  query = url.substr(pos+1);
+  var result = {};
+  query.split("&").forEach(function(part) {
+    if(!part) return;
+    part = part.split("+").join(" "); // replace every + with space, regexp-free version
+    var eq = part.indexOf("=");
+    var key = eq>-1 ? part.substr(0,eq) : part;
+    var val = eq>-1 ? decodeURIComponent(part.substr(eq+1)) : "";
+    var from = key.indexOf("[");
+    if(from==-1) result[decodeURIComponent(key)] = val;
+    else {
+      var to = key.indexOf("]");
+      var index = decodeURIComponent(key.substring(from+1,to));
+      key = decodeURIComponent(key.substring(0,from));
+      if(!result[key]) result[key] = [];
+      if(!index) result[key].push(val);
+      else result[key][index] = val;
+    }
+  });
+  return result;
+}
+
 function getNextSummary() {
   var driver = new webdriver.Builder()
       .forBrowser('firefox')
       .build();
 
   var url = projects.pop();
+  var params = getJsonUrlParams(url);
+  console.log(params);
+  console.log("Reading user " + params["Username"] + " project " + params["ProjectName"]);
+
   driver.get(url);
-
   driver.sleep(5000);
-
   driver.executeScript(function() {
     /* global world */
     var ide = world.children[0];
